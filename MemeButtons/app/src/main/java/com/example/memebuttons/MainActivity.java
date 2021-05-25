784df46b8c35ff8d717f83c73feab88f55752238
Alessio Pietroforte <83708624+AlessioPietroforte@users.noreply.github.com>
2021-05-25 18:23:37 +0200

    Prova inserimento progetto in repo
    Speriamo funzioni :D

---
+++ MemeButtons/app/src/main/java/com/example/memebuttons/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.memebuttons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button but1 = findViewById(R.id.but1);
+        final MediaPlayer sound1 = MediaPlayer.create(this, R.raw.fuckyou);
+
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                sound1.start();
+
+            }
+        });
+
+
+
+
+    }
+}
